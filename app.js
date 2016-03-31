a50e667c3acddf92e0e617958e27be78c71e7d2a
Wolf <contact@nickwolf.com.au>
2016-03-31 12:28:00 +1100

    updated game to use colours rather than tokens

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ board = ['', '', '', '', '', '', '', '', '']
 var moveBtn = document.getElementById("move")
 var gameBoard = document.querySelector("#gameBoard")
 
+var xColor = 'White';
+var oColor = 'Black';
 
-// function checkWin() {
-//     if (checkWin[0] === checkWin[1] && checkWin[2] === checkWin[1]) {
-//         if (checkWin[0] !== '') {
-//             console.log("t" + checkWin[0])
-//         }
-//     }
-// }
+var colorPicker = document.getElementById("myBtn")
+
+var playerTurn = 'X';
 
 winningCombo = [
     [0, 1, 2],
@@ -23,67 +21,104 @@ winningCombo = [
     [2, 4, 6]
 ]
 
-function checkWinner(array) {
-    // console.log('check winner');
-    // console.log([array[0], array[1]].join())
-    // console.log([array[0], array[1], array[2]].join(''));
+function colorDetermine(playerTurn){
+return (playerTurn === 'X') ? xColor : oColor
+}
 
+// document.getElementById('turn').innerHTML = 'White'
+function checkWinner(array) {
+ // console.log([array[0], array[1], array[2]].join(''));
     for (i = 0; i < winningCombo.length; i++) {
         // console.log(array[winningCombo[i][0]]);
-        if ([array[winningCombo[i][0]], array[winningCombo[i][1]], array[winningCombo[i][2]]].join('') === 'XXX') {
-            console.log("The winner is X");
-        } else if ([array[winningCombo[i][0]], array[winningCombo[i][1]], array[winningCombo[i][2]]].join() === 'OOO') {
-            console.log("The winner is O");
+        Line = [array[winningCombo[i][0]], array[winningCombo[i][1]], array[winningCombo[i][2]]].join('')
+        if (Line === 'XXX') {
+            alert("The winner is " + xColor);
+          resetBoard();
+        } else if (Line === 'OOO') {
+            alert("The winner is " + oColor);
+            resetBoard();
         }
 
     }
+    if (board.join('').length === 9) {
+        alert("it's a tie!")
+        resetBoard()
+    }
+    document.getElementById('turn').innerHTML = colorDetermine(playerTurn);
 }
 
+function resetBoard() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    for (var i = 0; i < 9; i++) {
+    document.getElementById(i).style = "background-color : 'grey'";
+}
 
+    refreshBoard();
+}
 
 function refreshBoard() {
     // console.log('clicked');
-    gameBoard.innerHTML = ''
+
     for (var i = 0; i < board.length; i++) {
-        // console.log(board[i])
-        var box = document.createElement('div');
-        box.innerHTML = board[i];
-
-
-        // console.log(document.querySelector("#gameBoard"))
-        gameBoard.appendChild(box);
-
-
-
+        document.getElementById(i).innerHTML = board[i]
     }
 }
 
 moveBtn.addEventListener("click", refreshBoard);
 
-Turn = 'X';
+var playerColor = 'red'
 
 gameBoard.addEventListener('click', function(event) {
-    // console.log(event.target);
-    // console.log(event);
     if (event.target.innerHTML === '') {
-        event.target.innerHTML = Turn;
-        board[Number(event.target.id)] = Turn
-        if (Turn === 'X') {
-            Turn = 'O'
-        }
-        else {
-          Turn = 'X'
+        event.target.style = "background-color :" + colorDetermine(playerTurn);
+        board[Number(event.target.id)] = playerTurn
+        event.target.innerHTML = playerTurn;
+        console.log(playerTurn);
+        if (playerTurn === 'X') {
+            playerTurn = 'O'
+        } else {
+            playerTurn = 'X'
         }
     }
     checkWinner(board)
-    //   if (event.target.tagName === "DIV"){
-
-    //   if (event.target.className === 'done'){
-    //     event.target.className = '';
-    //   }
-    //   else if (event.target.className === ''){
-    //   event.target.className = 'done';
-    // }
 })
 
-checkWinner(board)
+
+function goodColor(colour)
+{
+   var colour2="";
+   var result=true;
+   var e=document.getElementById('mydiv');
+   e.style.borderColor="";
+   e.style.borderColor=colour;
+   colour2=e.style.borderColor;
+   if (colour2.length==0){result=false;}
+   e.style.borderColor="";
+   return result;
+}
+
+
+colorPicker.addEventListener('click', function() {
+  if (board.join('').length > 0){
+    alert('you cannot change color mid game.')
+    return false
+  }
+  color1 = document.getElementById("color1").value.toUpperCase();
+  color2 = document.getElementById("color2").value.toUpperCase();
+  if (color1 === color2) {
+    return false;
+  }
+  
+  if (goodColor(color1) === true) {
+    console.log('good color 1 attempt')
+    xColor = document.getElementById("color1").value.toUpperCase();
+  }
+  if (goodColor(color2) === true) {
+    oColor = document.getElementById("color2").value.toUpperCase();
+  }
+}
+)
+
+
+
+refreshBoard(board)
